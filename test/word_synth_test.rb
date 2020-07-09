eb29cb4252e1de5510ae764648e2caa8b4ccc972
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-07-09 16:29:54 +0900

    test word synth

--- test/word_synth_test.rb
+++ test/word_synth_test.rb
@@ -4,7 +4,12 @@ require './lib/effect'
 
 class WordSynthTest < Minitest::Test
   def test_play
-    assert WordSynth
-    assert Effect
+    synth = WordSynth.new
+
+    synth.add_effect(&Effect.echo(2))
+    synth.add_effect(&Effect.loud(3))
+    synth.add_effect(&Effect.reverse)
+
+    assert_equal('!!!YYBBUURR !!!SSII !!!!!NNUUFF', synth.play('Ruby is fun!'))
   end
 end
