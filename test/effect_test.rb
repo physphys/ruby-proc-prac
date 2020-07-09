b9a5f70ad954415bdf2d6391e87be34905fe397b
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-07-09 16:34:27 +0900

    test effect module

--- test/effect_test.rb
+++ test/effect_test.rb
@@ -3,6 +3,17 @@ require './lib/effect'
 
 class EffectTest < Minitest::Test
   def test_reverse
-    assert Effect
+    effect = Effect.reverse
+    assert_equal('ybuR si !nuf', effect.call('Ruby is fun!'))
+  end
+
+  def test_loud
+    effect = Effect.loud(3)
+    assert_equal('RUBY!!! IS!!! FUN!!!!', effect.call('Ruby is fun!'))
+  end
+
+  def test_echo
+    effect = Effect.echo(3)
+    assert_equal('RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call('Ruby is fun!'))
   end
 end
