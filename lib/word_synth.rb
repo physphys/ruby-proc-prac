8c67a43eb8f3025c0783373a81a7e138f5004993
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-07-09 14:54:25 +0900

    init

---
+++ lib/word_synth.rb
@@ -0,0 +1,15 @@
+class WordSynth
+  def initialize
+    @effects = []
+  end
+
+  def add_effect(effect)
+    @effects << effect
+  end
+
+  def play(words)
+    @effects.inject(words).do |word, effect|
+      effect.call(word)
+    end
+  end
+end
